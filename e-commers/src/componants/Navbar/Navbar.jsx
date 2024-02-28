68f888c5530815a77d61dcd2746413e10da522a8
ziad <helmyzez@gmail.com>
2024-02-29 00:01:03 +0200

    updateNav

--- e-commers/src/componants/Navbar/Navbar.jsx
+++ e-commers/src/componants/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../Accets/images/freshcart-logo.svg'
 import { UserContext } from '../../Context/UserContext'
-
+import style from '../Navbar/Navbar.module.css'
 
 export default function Navbar() {
 
@@ -19,35 +19,35 @@ function Logout(){
 
   return <>
   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+  <div className="container-fluid ">
     <Link className="navbar-brand" to='/'>
-      <h2 className='h3 text-main fw-bolder'>ZIAD SHop</h2>
+      <h2 className={` ${style.logo}`}>Guzel</h2>
       {/* <img src={logo} alt="fresh market logo" /> */}
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav me-auto">
+      <ul className="navbar-nav m-auto">
         
         {userToken !== null ? <>
           <li className="nav-item">
-          <Link className="nav-link" to={'/'}>Home</Link>
+          <Link className={`nav-link fw-bold ${style.navhov}` }  to={'/'}>Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={'/products'}>Products</Link>
+          <Link className={`nav-link fw-bold ${style.navhov}` } to={'/products'}>Products</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={'/categories'}>Categories</Link>
+          <Link className={`nav-link fw-bold ${style.navhov}` } to={'/categories'}>Categories</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={'/brands'}>Brands</Link>
+          <Link className={`nav-link fw-bold ${style.navhov}` } to={'/brands'}>Brands</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={'/wishlist'}>Wish list</Link>
+          <Link className={`nav-link fw-bold ${style.navhov}` } to={'/wishlist'}>Wish list</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={'/cart'}>Cart</Link>
+          <Link className={`nav-link fw-bold ${style.navhov}` } to={'/cart'}>Cart</Link>
         </li>
         </> : "" }
         
@@ -55,18 +55,18 @@ function Logout(){
         
       </ul>
       <ul className="navbar-nav ms-auto">
-      <li className="nav-item d-flex align-items-center">
-        <a href="https://www.facebook.com/zezo.helmy.52012/" target='_blank'><i className='fab fa-facebook mx-2'></i></a>
-          <a href="https://github.com/zyadessa2"><i className='fab fa-github mx-2'></i></a>
-          <a href="https://www.instagram.com/zyad.helmy/"><i className='fab fa-instagram mx-2'></i></a>
+      <li className={`nav-item d-flex fs-4 ${style.sochover} align-items-center`}>
+        <a className={style.sochover} href="https://www.facebook.com/zezo.helmy.52012/" target='_blank'><i className='fab fa-facebook mx-2'></i></a>
+          <a className={style.sochover} href="https://github.com/zyadessa2"><i className='fab fa-github mx-2'></i></a>
+          <a className={style.sochover} href="https://www.instagram.com/zyad.helmy/"><i className='fab fa-instagram mx-2'></i></a>
         </li>
         {userToken !== null ? <>
           <li className="nav-item">
-          <span className="nav-link cursor-pointer" onClick={()=>Logout()}>Logout</span>
+          <span className={`nav-link cursor-pointer ${style.logout}`} onClick={()=>Logout()}>Logout</span>
           </li>
         </>:<>
         <li class="nav-item">
-          <Link className="nav-link" to={'/login'}>Login</Link>
+          <Link className={`nav-link ${style.logout}`} to={'/login'}>Login</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to={'/register'}>Register</Link>
