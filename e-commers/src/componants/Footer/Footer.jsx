c5f5d71c1143c2bfb262d71521b9e1a5467ec7a4
ziad <helmyzez@gmail.com>
2024-03-01 10:59:05 +0200

    updateFooter

--- e-commers/src/componants/Footer/Footer.jsx
+++ e-commers/src/componants/Footer/Footer.jsx
@@ -1,45 +1,79 @@
 import React from 'react';
-import styles from './Footer.module.css';
+import style from './Footer.module.css';
 import { Link } from 'react-router-dom';
-
+import img1 from '../../Accets/images/slider-image-1.jpeg'
+import img2 from '../../Accets/images/slider-image-2.jpeg'
+import img3 from '../../Accets/images/slider-image-3.jpeg'
+import img4 from '../../Accets/images/blog-img-2.jpeg'
+import img5 from '../../Accets/images/blog-img-1.jpeg'
+import img6 from '../../Accets/images/slider-image-1.jpeg'
 
 export default function Footer() {
 
   return <>
-    <footer className='text-center text-white  mt-5 bg-black mb-0 pb-0'>
-      <div className="container">
-        <div className="row p-5">
-          <div className="col-md-3 mb-2 ">
-          <Link className="nav-link text-white" to={'/'}>Home</Link>
+    <footer className={style.footer}>
+    <div class={style.container}>
+        <div class={style.box}>
+          <h3>Guzel</h3>
+          {/* <ul class={style.social}>
+            <li>
+              <a href="#" class={style.facebook}>
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class={style.twitter}>
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class={style.youtube}>
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul> */}
+          <p class={style.text}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          </p>
+        </div>
+        <div class={style.box}>
+          <ul class={style.links}>
+            <li><Link className={`nav-link fw-bold ${style.navhov}` }  to={'/'}>Home</Link></li>
+            <li><Link className={`nav-link fw-bold ${style.navhov}` } to={'/products'}>Products</Link></li>
+            <li><Link className={`nav-link fw-bold ${style.navhov}` } to={'/categories'}>Categories</Link></li>
+            <li><Link className={`nav-link fw-bold ${style.navhov}` } to={'/brands'}>Brands</Link></li>
+            {/* <li><Link className={`nav-link fw-bold ${style.navhov}` } to={'/wishlist'}>Wish list</Link></li> */}
+            {/* <li><Link className={`nav-link fw-bold ${style.navhov}` } to={'/cart'}>Cart</Link></li> */}
+          </ul>
+        </div>
+        <div class={style.box}>
+          {/* <div class={style.line}>
+            <i class="fas fa-map-marker-alt fa-fw"></i>
+            <div class={style.info}>Egypt, Giza, Inside The Sphinx, Room Number 220</div>
+          </div> */}
+          <div class={style.line}>
+            <i class="far fa-clock fa-fw"></i>
+            <div class={style.info}>Business Hours: From 10:00 To 18:00</div>
           </div>
-          <div className="col-md-3 mb-2">
-          <Link className="nav-link text-white" to={'/products'}>Products</Link>
-          </div>
-          <div className="col-md-3 mb-2">
-          <Link className="nav-link text-white" to={'/categories'}>Categories</Link>
-          </div>
-          <div className="col-md-3 mb-2">
-          <Link className="nav-link text-white" to={'/cart'}>Cart</Link>
+          <div class={style.line}>
+            <i class="fas fa-phone-volume fa-fw"></i>
+            <div class={style.info}>
+              <span>+201066958945</span>
+              {/* <span>+20198765432</span> */}
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-11 text-center m-auto">
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo
-            s, alias quaerat? Expedita re
-            rum cum minus ad itaque recusandae blanditiis eaque.</p>
-          </div>
-          <div className="socail-footer pb-1 m-2 cursor-pointer">
-            <i className='fab fa-facebook mx-2'></i>
-            <i className='fab fa-twitter mx-2'></i>
-            <i className='fab fa-instagram mx-2'></i>
-            <i className='fab fa-tiktok mx-2'></i>
-            <i className='fab fa-youtube mx-2'></i>
-          </div>
+        <div class={style.footerGallery}>
+          <img decoding="async" src={img1} alt="" />
+          <img decoding="async" src={img2}alt="" />
+          <img decoding="async" src={img3} alt="" />
+          <img decoding="async" src={img4} alt="" />
+          <img decoding="async" src={img5} alt="" />
+          <img decoding="async" src={img6} alt="" />
         </div>
       </div>
-      <div className="bottom-footer bg-dark p-2 w-100">
-          <p>@2023 copyright ziad essa</p>
-      </div>
+      <p class={style.copyright}>Made With &lt;3 By Ziad</p>
     </footer>
+    
   </>
 }
